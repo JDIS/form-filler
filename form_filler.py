bc0fd7b5f84686183ff95bffbb2f37e6dfb1dc7e
Ariadne Paradis <88592270+ThreadGiver@users.noreply.github.com>
2024-12-13 16:05:04 -0500

    Added file selector and automatic image sizing

--- form_filler.py
+++ form_filler.py
@@ -1,99 +1,224 @@
 import datetime
 import pymupdf
-from config import *
+from pymupdf import Pixmap
+import os
+from os.path import isfile, join
 
 
-def check(page, yes_point, no_point, prompt):
-    if input(prompt).lower() == "y":
-        page.insert_text(yes_point, "X")
-    else:
-        page.insert_text(no_point, "X")
 
 
-def remboursement():
-    form = pymupdf.open(purchase_form_filename)
-    page = form[0]
+class Form_Filler:
+    from config import (
+        user_name,
+        user_email,
+        user_phone,
+        nom_tresorie,
+        default_gas_rate,
+        do_sign,
+        signature_filename,
+        purchase_form_filename,
+        gas_form_filename
+    )
 
-    page.insert_text((170, 97), "JDIS")
-    page.insert_text((78, 140), "X")
-    name = input("Nom pour remboursement: ")
-    page.insert_text((235, 183), name)
-    page.insert_text((235, 219), user_name)
-    page.insert_text((165, 245), user_email)
-    page.insert_text((415, 245), user_phone)
-    page.insert_text((200, 285), input("Montant à rembourser: ") + "$")
-    check(page, (329, 273), (329, 299), "Inscrit Accès D? (Y/N): ")
-    check(page, (227, 347), (287, 347), "Pièce justificative originale? (Y/N): ")
-    check(page, (227, 389), (287, 389), "Activité de financement? (Y/N): ")
-    check(page, (227, 416), (287, 416), "Revenu lié à la dépense? (Y/N): ")
-    project = input("Projet/activité si connu: ")
-    page.insert_text((210, 455), project)
-    while 0 >= page.insert_textbox(
-        (210, 470, 540, 550),
-        input("Description de la dépense: "),
-        lineheight=2.25,
-    ):
-        print("Despcription too long")
-    time = datetime.datetime.now()
-    page.insert_text((105, 570), time.strftime("%d-%b-%Y"))
-    if sign:
-        page.insert_image((355, 533, 400, 583), filename=signature_filename)
-    page.insert_text((255, 630), nom_presidence)
-    page.insert_text((355, 630), nom_tresorie)
-    while True:
-        filename = input("Nom du fichier à ajouter? (Laisser vide si aucun): ")
-        if not filename:
-            break
-        form.insert_file(filename)
+    form = None
+    page = None
+    base_filename = ""
+    pagefill_count = 0
+    expected_file_extensions = [".pdf", ".jpg", ".png"]
+    files = [
+        f
+        for f in os.listdir(os.getcwd())
+        if (
+            isfile(join(os.getcwd(), f))
+            and os.path.splitext(f)[1] in [".pdf", ".jpg", ".png"]
+        )
+    ]
 
-    form.save(f"remboursement {project} {name} {time.strftime('%Hh%M')}.pdf")
+    def check(s, yes_point, no_point, prompt, default="n"):
+        target = no_point
+        if input(prompt).lower() == "y" or default != "n":
+            target = yes_point
+        s.page.insert_text(target, "X")
+
+    def remboursement(s):
+        s.page.insert_text((170, 97), "JDIS")
+        s.page.insert_text((78, 140), "X")
+        name = input("Nom pour remboursement: ")
+        s.page.insert_text((235, 183), name)
+        s.page.insert_text((235, 219), s.user_name)
+        s.page.insert_text((165, 245), s.user_email)
+        s.page.insert_text((415, 245), s.user_phone)
+        s.page.insert_text((200, 285), input("Montant à rembourser: ") + "$")
+        s.check((329, 273), (329, 299), "Inscrit Accès D? (Y/[N]): ")
+        s.check((227, 347), (287, 347), "Pièce justificative originale? ([Y]/N): ", "y")
+        s.check((227, 389), (287, 389), "Activité de financement? (Y/[N]): ")
+        s.check((227, 416), (287, 416), "Revenu lié à la dépense? (Y/[N]): ")
+        project = input("Projet/activité si connu: ")
+        s.page.insert_text((210, 455), project)
+        s.textfield((210, 470, 540, 550), "Description de la dépense: ")
+        time = datetime.datetime.now()
+        s.page.insert_text((105, 570), time.strftime("%d-%b-%Y"))
+
+        s.sign((355, 533, 400, 583))
+        s.page.insert_text((255, 630), s.nom_presidence)
+        s.page.insert_text((355, 630), s.nom_tresorie)
+        s.add()
+        s.form.save(f"remboursement {project} {name} {time.strftime('%Hh%M')}.pdf")
+
+    def gas(s):
+        s.page.insert_text((170, 97), "JDIS")
+        s.page.insert_text((78, 140), "X")
+        name = input("Nom de la personne à rembourser: ")
+        s.page.insert_text((255, 183), name)
+        s.page.insert_text((255, 212), s.user_name)
+        s.page.insert_text((110, 240), s.user_email + "@usherbrooke.ca")
+        s.page.insert_text((415, 240), s.user_phone)
+        s.page.insert_text((200, 280), input("Adresse de destination: "))
+        distance = float(input("Distance: "))
+        taux_essence = s.default_gas_rate
+        # taux_essence = float(input("Taux essence: "))
+        s.page.insert_text((128, 335), f"{distance}km")
+        s.page.insert_text((370, 337), f"{taux_essence}$")
+        s.page.insert_text((200, 362), f"{distance * taux_essence:.2f}$")
+        s.check((227, 409), (287, 406), "Activité de financement? (Y/[N]): ")
+        s.check((227, 436), (287, 433), "Revenu lié à la dépense? (Y/[N]): ")
+        project = input("Projet/activité si connu: ")
+        s.page.insert_text((210, 475), project)
+        s.textfield((210, 505, 540, 585), "Raison du déplacement: ")
+        time = datetime.datetime.now()
+        s.page.insert_text((105, 600), time.strftime("%d-%b-%Y"))
+        s.sign((435, 585, 480, 660))
+        s.form.save(
+            f"remboursement essence {project} {name} {time.strftime('%Hh%M')}.pdf"
+        )
+
+    def textfield(s, rect, prompt):
+        while 0 >= s.page.insert_textbox(rect, input(prompt), lineheight=2.25):
+            print("Textfield too long")
+
+    def sign(s, rect):
+        if s.do_sign:
+            s.page.insert_image(rect, filename=s.signature_filename)
+
+    def open(s):
+        s.form = pymupdf.open(s.base_filename)
+        s.page = s.form[0]
+
+    def add(s):
+        s.print_cwd_files()
+        image_list, pdf_file_list = s.get_image_file_list()
+        s.add_pdf_files(pdf_file_list)
+        image_ratios = s.get_image_ratios(image_list)
+        image_list.sort(key=(lambda i: image_ratios[str(i)]), reverse=True)
+        s.add_images(image_list, image_ratios)
+
+    def add_pdf_files(s, pdf_file_list):
+        for p in pdf_file_list:
+            s.add_file(s.files[p])
+
+    def get_file_index_list(s):
+        return list(
+            map(lambda i: int(i), input("List files to add (ie: 1,3,5,6): ").split(","))
+        )
+
+    def get_image_ratios(s, image_list):
+        return {
+            str(i): (Pixmap(s.files[i]).width / Pixmap(s.files[i]).height)
+            for i in image_list
+        }
+
+    def add_images(s, image_list, image_ratios):
+        for i in image_list:
+            if image_ratios[str(i)] <= 0.333:
+                s.add_halfpage_image(s.files[i])
+            else:
+                s.add_quarterpage_image(s.files[i])
+
+    def print_cwd_files(s):
+        i = 0
+        print("\n Working directory files")
+        for file in s.files:
+            file_extension = os.path.splitext(file)[1]
+            if file_extension in Form_Filler.expected_file_extensions:
+                print(f"[{i}] {file}")
+                i += 1
+
+    def get_image_file_list(s):
+        image_list = []
+        pdf_file_list = []
+        for i in s.get_file_index_list():
+            filename = s.files[i]
+            file_extension = os.path.splitext(filename)[1]
+            if file_extension == ".pdf" and pymupdf.open(filename).page_count > 1:
+                pdf_file_list.append(i)
+            else:
+                image_list.append(i)
+        return (image_list, pdf_file_list)
+
+    def add_file(s, file_name=""):
+        if not file_name:
+            file_name = input("File name: ")
+        s.form.insert_file(file_name)
+
+    def add_image(s, file_name=""):
+        if not file_name:
+            file_name = input("File name: ")
+
+    def add_fullpage_image(s, file_name=""):
+        if not file_name:
+            file_name = input("File name: ")
+        s.page = s.form.new_page(-1, *pymupdf.paper_size("letter"))
+        s.page.insert_image((0, 0, 595, 842), filename=file_name)
+        s.pagefill_count = 0
+
+    def add_halfpage_image(s, file_name=""):
+        if not file_name:
+            file_name = input("File name: ")
+        match s.pagefill_count:
+            case 1 | 2:
+                s.page.insert_image((298, 0, 595, 842), filename=file_name)
+                s.pagefill_count = 0
+            case _:
+                s.page = s.form.new_page(-1, *pymupdf.paper_size("letter"))
+                s.page.insert_image((0, 0, 297, 842), filename=file_name)
+                s.pagefill_count = 2
+
+    def add_quarterpage_image(s, file_name=""):
+        if not file_name:
+            file_name = input("File name: ")
+        match s.pagefill_count:
+            case 1:
+                s.page.insert_image((0, 421, 287, 842), filename=file_name)
+            case 2:
+                s.page.insert_image((298, 0, 595, 421), filename=file_name)
+            case 3:
+                s.page.insert_image((298, 421, 595, 842), filename=file_name)
+            case _:
+                s.page = s.form.new_page(-1, *pymupdf.paper_size("letter"))
+                s.page.insert_image((0, 0, 297, 421), filename=file_name)
+                s.pagefill_count = 0
+        s.pagefill_count += 1
+
+    def test(s):
+        s.form = pymupdf.open(s.purchase_form_filename)
+        s.add_fullpage_image("parkscreenshotsg.png")
+        s.form.save("test.pdf")
+
+    def select(s):
+        match input("Type de formulaire? [Remboursement|Essence]: ").lower()[0]:
+            case "r":
+                s.base_filename = s.purchase_form_filename
+                s.open()
+                s.remboursement()
+            case "e":
+                s.base_filename = s.gas_form_filename
+                s.open()
+                s.gas()
+            case "t":
+                s.test()
+            case _:
+                print("Mauvaise option")
+                s.select()
 
 
-def gas():
-    form = pymupdf.open(gas_form_filename)
-    page = form[0]
-
-    page.insert_text((170, 97), "JDIS")
-    page.insert_text((78, 140), "X")
-    name = input("Nom de la personne à rembourser: ")
-    page.insert_text((255, 183), name)
-    page.insert_text((255, 212), user_name)
-    page.insert_text((110, 240), user_email + "@usherbrooke.ca")
-    page.insert_text((415, 240), user_phone)
-    page.insert_text((200, 280), input("Adresse de destination: "))
-    distance = float(input("Distance: "))
-    taux_essence = default_gas_rate
-    # taux_essence = float(input("Taux essence: "))
-    page.insert_text((128, 335), f"{distance}km")
-    page.insert_text((370, 337), f"{taux_essence}$")
-    page.insert_text((200, 362), f"{distance * taux_essence:.2f}$")
-    check(page, (227, 409), (287, 406), "Activité de financement? (Y/N): ")
-    check(page, (227, 436), (287, 433), "Revenu lié à la dépense? (Y/N): ")
-    project = input("Projet/activité si connu: ")
-    page.insert_text((210, 475), project)
-    while 0 >= page.insert_textbox(
-        (210, 505, 540, 585),
-        input("Raison du déplacement: "),
-        lineheight=2.25,
-    ):
-        print("Despcription too long")
-    time = datetime.datetime.now()
-    page.insert_text((105, 600), time.strftime("%d-%b-%Y"))
-    if sign:
-        page.insert_image((435, 585, 480, 660), filename=signature_filename)
-
-    form.save(f"remboursement essence {project} {name} {time.strftime('%Hh%M')}.pdf")
-
-
-def select():
-    match input("Type de formulaire? [Remboursement|Essence]:").lower()[0]:
-        case "r":
-            remboursement()
-        case "e":
-            gas()
-        case _:
-            print("Mauvaise option")
-            select()
-
-
-select()
+Form_Filler().select()
