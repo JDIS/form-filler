8441852c56f7d851a63b5d8eef50a9e616ad068f
Ariadne Paradis <88592270+ThreadGiver@users.noreply.github.com>
2025-05-07 13:02:41 -0400

    Update config.py to be applicable to more groups

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ user_name = "ton nom"
 user_email = "ton courriel avant le @usherbrooke"  # might have to adjust position
 user_phone = "ton num"
 sign = False  # true si t'es trésorier(e)
+nom_tresorie = "nom tres du groupe"
+nom_presidence = "nom pres du groupe"
+nom_groupe = "nom de ton groupe"
+default_gas_rate = 0.48
 
 purchase_form_filename = "E24-Remboursement-Achat.pdf"
 gas_form_filename = "A22-Formulaire de frais km.pdf"
 deposit_form_filename = "A22-Formulaire-dépôt-dargent.pdf"
 signature_filename = "signature.png"
-nom_tresorie = "Ariadne Paradis"
-nom_presidence = "Béatrice Gagnon"
-default_gas_rate = 0.20
